11639e3982bf33412d253bb7963d8531185693f9
Haritha-R-02 <haritharamesh2007@gmail.com>
2026-01-19 23:47:12 +0530

    Create ChatClient.java

---
+++ ChatClient.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class ChatClient {
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 1234);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner scanner = new Scanner(System.in);
+
+            // Thread to read messages from server
+            new Thread(() -> {
+                try {
+                    String msg;
+                    while ((msg = in.readLine()) != null) {
+                        System.out.println(msg);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Connection closed");
+                }
+            }).start();
+
+            // Send messages
+            while (true) {
+                String message = scanner.nextLine();
+                out.println(message);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Unable to connect to server");
+        }
+    }
+}
